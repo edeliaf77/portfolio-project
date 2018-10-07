4ba145b9d20cb36f10da9a6f6be2c34d81347785
Ed Fresco <edeliaf@gmail.com>
2018-10-06 22:16:06 -0600

    Jobs Apps running

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'portfoliodb',
+        'USER':'eqf4J',
+        'PASSWORD':'Matt;2237',
+        'HOST':'localhost',
+        'PORT':'5433',
     }
 }
 
