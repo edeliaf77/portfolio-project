e1421f6325f1ff9c208a7dd578cd71b0be5ba45b
Ed Fresco <edeliaf@gmail.com>
2018-10-06 19:43:36 -0600

    Our First

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -1,4 +1,3 @@
-
 """portfolio URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
@@ -14,17 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
 from django.contrib import admin
-from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
-# from django.conf import settings
-# from django.conf.urls.static import static
-import jobs.views
+from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', jobs.views.home, name='home')),
-    path('blog/', include('blog.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
